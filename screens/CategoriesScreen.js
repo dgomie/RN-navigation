a89f054facf79177f993396b869d31309d38f4bb
Danny <danny.f.gomez@gmail.com>
2024-11-24 18:08:59 -0500

    create catergories screen file, import dummy data and models from course

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,8 @@
+import {CATEGORIES} from "../data/dummy-data";
+import {FlatList} from "react-native";
+
+export default function CategoriesScreen(){
+    return <>
+    <FlatList data={CATEGORIES} renderItem={}
+    </>
+}
