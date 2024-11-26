88297a025d92c9961308409a65f6e88b8df39418
Danny <danny.f.gomez@gmail.com>
2024-11-26 12:40:03 -0500

    add navigation handler to navigate to meals overview screen when griditem is pressed

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,32 @@
-import {CATEGORIES} from "../data/dummy-data";
-import {FlatList} from "react-native";
-import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from '../data/dummy-data';
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
 
-export default function CategoriesScreen() {
-    function renderCategoryItem(itemData) {
-        return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
+export default function CategoriesScreen({ navigation }) {
+ 
+
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+        navigation.navigate('MealsOverview');
     }
 
-    return <>
-        <FlatList
-            data={CATEGORIES}
-            keyExtractor={(item) => item.id}
-            renderItem={renderCategoryItem}
-            numColumns={2}
-        />
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
+  return (
+    <>
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+      />
     </>
+  );
 }
