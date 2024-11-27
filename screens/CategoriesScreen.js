11415dcb6144379c1f52f1b3848a97eed585713b
Danny <danny.f.gomez@gmail.com>
2024-11-27 18:15:16 -0500

    pass params through navigate to mealsoverviewscreen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,11 +3,9 @@ import { FlatList } from 'react-native';
 import CategoryGridTile from '../components/CategoryGridTile';
 
 export default function CategoriesScreen({ navigation }) {
- 
-
   function renderCategoryItem(itemData) {
     function pressHandler() {
-        navigation.navigate('MealsOverview');
+      navigation.navigate('MealsOverview', { categoryId: itemData.item.id,  });
     }
 
     return (
