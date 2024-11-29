3aee22c542b0b53ceee88b2dd9cfa2af1d0f9564
Danny <danny.f.gomez@gmail.com>
2024-11-28 19:03:19 -0500

    add image to mealitem, create styles for image and text

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,9 +1,26 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, Image, StyleSheet } from 'react-native';
 
-export default function MealItem({ title }) {
+export default function MealItem({ title, imageUrl }) {
   return (
     <View>
-      <Text>{title}</Text>
+      <Pressable>
+        <View>
+          <Image source={{ uri: imageUrl }} style={styles.image} />
+        </View>
+        <Text style={styles.title}>{title}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 200
+    },
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18
+    }
+})
