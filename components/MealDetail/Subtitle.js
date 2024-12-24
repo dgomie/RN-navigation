0eb06d71ea5db29b45e84a8d4bc15183f73c96c3
Danny <danny.f.gomez@gmail.com>
2024-12-24 17:56:33 -0500

    add styling to subtitle component

--- components/MealDetail/Subtitle.js
+++ components/MealDetail/Subtitle.js
@@ -10,6 +10,20 @@ function Subtitle() {
 export default Subtitle;
 
 const styles = StyleSheet.create({
-  subtitleContainer: {},
-  subtitle: {},
+    subtitle: {
+        color: 'slate',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        borderBottomColor: 'white',
+        borderBottomWidth: 2,
+      },
+      subtitleContainer: {
+        borderBottomColor: 'white',
+        borderBottomWidth: 2,
+        marginHorizontal: 24,
+        marginVertical: 4,
+        margin: 4,
+        padding: 6,
+      },
 });
