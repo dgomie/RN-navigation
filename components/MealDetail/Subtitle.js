ffaf1ef6042d205fb4ee9b6da5f28b15cc15eb4f
Danny <danny.f.gomez@gmail.com>
2024-12-24 17:57:21 -0500

    add children prop to subtitle component

--- components/MealDetail/Subtitle.js
+++ components/MealDetail/Subtitle.js
@@ -1,8 +1,8 @@
 import { View, Text, StyleSheet } from 'react-native';
-function Subtitle() {
+function Subtitle({ children }) {
   return (
     <View style={styles.subtitleContainer}>
-      <Text style={styles.subtitle}>Steps</Text>
+      <Text style={styles.subtitle}>{children}</Text>
     </View>
   );
 }
@@ -10,20 +10,20 @@ function Subtitle() {
 export default Subtitle;
 
 const styles = StyleSheet.create({
-    subtitle: {
-        color: 'slate',
-        fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        borderBottomColor: 'white',
-        borderBottomWidth: 2,
-      },
-      subtitleContainer: {
-        borderBottomColor: 'white',
-        borderBottomWidth: 2,
-        marginHorizontal: 24,
-        marginVertical: 4,
-        margin: 4,
-        padding: 6,
-      },
+  subtitle: {
+    color: 'slate',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    borderBottomColor: 'white',
+    borderBottomWidth: 2,
+  },
+  subtitleContainer: {
+    borderBottomColor: 'white',
+    borderBottomWidth: 2,
+    marginHorizontal: 24,
+    marginVertical: 4,
+    margin: 4,
+    padding: 6,
+  },
 });
