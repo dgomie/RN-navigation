f7d2397d3c25f03bd3c9369ab1b12df127e48add
Danny <danny.f.gomez@gmail.com>
2024-12-30 22:34:52 -0500

    debug floating code in IconButton

--- components/IconButton/IconButton.js
+++ components/IconButton/IconButton.js
@@ -4,7 +4,7 @@ import { Ionicons } from '@expo/vector-icons';
 function IconButton({onPress}) {
   return (
     <Pressable onPress={onPress} >
-      <Ionicons name="star" size={24} color="white" />{' '}
+      <Ionicons name="star" size={24} color="white" />
     </Pressable>
   );
 }
