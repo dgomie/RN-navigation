b145dee04c2df20eacf2aa23a43098e1b5409898
Danny <danny.f.gomez@gmail.com>
2024-12-30 22:33:03 -0500

    add on press property to IconButton

--- components/IconButton/IconButton.js
+++ components/IconButton/IconButton.js
@@ -1,9 +1,9 @@
 import { Pressable } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-function IconButton() {
+function IconButton({onPress}) {
   return (
-    <Pressable>
+    <Pressable onPress={onPress} >
       <Ionicons name="star" size={24} color="white" />{' '}
     </Pressable>
   );
